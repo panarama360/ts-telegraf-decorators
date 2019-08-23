a33cd2523d58f87af1ea11f50756db645bf5b11e
panarama360 <panarama360@gmail.com>
2019-08-23 07:32:13 +0300

    remove token bot :)

--- sample/app.ts
+++ sample/app.ts
@@ -3,7 +3,7 @@ import {buildBot} from "../src";
 import {Container} from "typedi";
 
 const bot = buildBot({
-    token: "705353703:AAHfcsPcOGSyraqVe-u123s5967Lv-X2Rl0",
+    token: process.env.BOT_TOKEN,
     container: Container,
     // bot: bot                 bot instance
     // session: session()       custom session
